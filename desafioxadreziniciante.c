ed44630228fb8f75311f6c3cb00051c9cc8f50cb
enzoestacio <202503130035@alunos.estacio.br>
2025-03-21 12:38:38 -0300

    Create desafioxadreziniciante.c

---
+++ desafioxadreziniciante.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+int main(){
+// Nomeando variaveis e iniciando a posição atual com 0 (0/8) de acordo com o lugar de um tabuleiro de xadrez
+    char torre;
+    char bispo;
+    char rainha;
+    int cima = 0, baixo = 0, esquerda = 0, direita = 0;
+
+    printf("***SEJA MUITO BEM VINDO AO SIMULADOR DE XADREZ***\n");
+    printf("Iniciando jogo:\n\n");
+
+    while (cima <= 4){
+        printf ("Torre está no bloco %d da posição cima, movendo torre uma casa para frente.\n", cima);
+    cima++;
+        printf("\nAgora, torre está na casa %d\n\n", cima);
+    }
+
+    for (direita, cima = 0; direita, cima <= 4; direita++,cima++){
+        printf("O bispo está no bloco %d da posção direita e %d da posição direita.\n", direita, cima);
+        printf("Movendo o %c para uma casa para frente e para a direita!\n", bispo);
+    }
+    printf("Agora o bispo está na posição direita %d e cima %d\n", direita, cima);
+
+
+
+
+    do{
+        printf("\n A rainha está na posição esquerda %d\n", esquerda);
+        printf("Movendo a um bloco para esquerda...\n" );
+
+        esquerda++;
+    
+        printf("a peça rainha está agora na posição esquerda : %d\n",  esquerda);
+
+    } while (esquerda <= 4);
+
+}
